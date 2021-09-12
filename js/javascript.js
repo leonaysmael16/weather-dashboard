9ca195f73a795ab2d88d2954ac7fef77b7374b47
leonaysmael16 <ysmealleona@gmail.com>
2021-09-11 22:34:47 -0700

    updates

---
+++ js/javascript.js
@@ -0,0 +1,9 @@
+const APIkey = "9ecbdf9d0dc12db2360cb9d8bc558329";
+// variables
+var searchCity = document.querySelector('#search-city');
+var city = document.querySelector('#city');
+var searchBtn = doceument.querySelector('#button');
+
+
+
+
