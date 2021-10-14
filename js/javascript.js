88098eb3685475356a30a6ba9699abdb400fc9b2
leonaysmael16 <ysmealleona@gmail.com>
2021-10-14 10:12:15 -0700

    edited id variables

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,26 @@
-const APIkey = "9ecbdf9d0dc12db2360cb9d8bc558329";
+var APIkey = "9ecbdf9d0dc12db2360cb9d8bc558329";
 // variables
-var searchCity = document.querySelector('#search-city');
-var city = document.querySelector('#city');
-var searchBtn = doceument.querySelector('#button');
+var search = document.querySelector('.search-button');
+var searchCity = document.querySelector('.search-city');
+var currentCity = document.querySelector('.current-city');
+var tempCurrent = document.querySelector('.current-temp');
+var humidityCurrent = document.querySelector('.Humidity');
+var curretWind = document.querySelector('.wind-speed');
+var currentUV = document.querySelector('.uv-index');
+var fiveDay = document.querySelector('.future-weather');
+// display current weather 
+
+// function weatherDisplay(event) {
+//     event.prevenDefault();
+//     if (citySearch.val().trim()!==""){
+//         city=searchCity.val().trim();
+//         currentWeather(city);
+//     }
+    
+// }
 
 
 
+
+// clears the search history 
 
